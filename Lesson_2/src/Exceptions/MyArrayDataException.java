dec059a2a34b26fa372461150b4e161910881344
Никита Егоркин <Bigyman91@gmail.com>
2019-03-06 02:25:39 +0300

    add

---
+++ Lesson_2/src/Exceptions/MyArrayDataException.java
@@ -0,0 +1,13 @@
+package Exceptions;
+
+public class MyArrayDataException extends Exception {
+    public int i;
+    public int j;
+
+    public MyArrayDataException(int i, int j) {
+        this.i = i;
+        this.j = j;
+
+    }
+
+}
---
+++ Lesson_2/src/Exceptions/MySizeArrayException.java
@@ -0,0 +1,8 @@
+package Exceptions;
+
+public class MySizeArrayException extends Exception {
+    public MySizeArrayException(String msg) {
+        super(msg);
+    }
+
+}
